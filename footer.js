fa4b5f06195ef2e6691a41cefe33ef4d239d7088
Vincent LALANNE <vlalanne90@gmail.com>
2013-05-30 22:54:39 +0200

    Added basic operations

---
+++ footer.js
@@ -0,0 +1,20 @@
+document.write('\
+		<footer id="footer" class="well well-small">\
+		<hr>\
+		<div class="container">\
+			<p class="pull-right">\
+				<a href="#top">Back to top</a>\
+			</p>\
+			<div class="links">\
+				<a href="https://github.com/ilaborie/Git--EGit-beginner-workshop/">GitHub</a>\
+			</div>\
+			Code licensed under the <a\
+				href="http://www.apache.org/licenses/LICENSE-2.0">Apache License\
+				v2.0</a>.<br> Based on <a\
+				href="http://twitter.github.com/bootstrap/">Bootstrap</a>. <br>\
+			Hosted on <a href="http://pages.github.com/">GitHub</a>. <br>\
+			Icons from <a href="http://fortawesome.github.com/Font-Awesome/">Font\
+				Awesome</a>.<br> Web fonts from <a\
+				href="http://www.google.com/webfonts">Google</a>. <br>\
+		</div>\
+	</footer>');
