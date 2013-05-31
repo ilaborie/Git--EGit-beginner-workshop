6f6509c8bf346e1f9c485c040c13d81369cb98db
Igor Laborie <ilaborie@gmail.com>
2013-05-31 22:09:22 +0200

    Index: add SVN & Git

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ document.write('\
 	<header>\
 		<div class="navbar">\
 	        <div class="navbar-inner">\
-	            <a class="brand" href="#">Git &amp; EGit beginner workshop</a>\
+	            <a class="brand" href="index.html">Git &amp; EGit beginner workshop</a>\
 					<ul class="nav">\
 						<li><a href="exo1.html">Installation & Configuration</a></li>\
 						<li><a href="exo2.html">Create a Repository</a></li>\
