8f7c90b2e767a9e047ea971f4887d169e8b5aef7
Igor Laborie <ilaborie@gmail.com>
2013-06-04 23:11:49 +0200

    exo8: create remote repo

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,11 +7,30 @@ $(function () {
     $(".nav li a").each(autoPrefix);
 
     // Optional
-    $("a.optional").each(function(idx, elt){
+    $("a.optional").each(function (idx, elt) {
         $(elt).prepend("<em>(Optional)</em> ");
     });
-});
 
-var autoPrefix = function(idx, elt){
-    $(elt).prepend("" +(idx+1) + ". ");
+    // Remote Repo
+    var $form = $("#createRemoteRepo");
+    $form.submit(function (e) {
+        $.ajax({
+            url: "http://192.168.42.1/repo/create",
+            data: $form.serialize(),
+            success: function (value) {
+                var $elt = $("#created");
+                $elt.show();
+                $elt.find("em").append("http://192.168.42.1/git/" + value);
+                $elt.find("a").attr("href", "/gitweb?p=" + value);
+            }
+        });
+
+        e.preventDefault();
+        return false;
+    });
+})
+;
+
+var autoPrefix = function (idx, elt) {
+    $(elt).prepend("" + (idx + 1) + ". ");
 };
