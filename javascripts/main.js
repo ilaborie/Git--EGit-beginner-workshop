7909d82b8e6d9cb690c978852ef3b78e0b1c9e62
Igor Laborie <ilaborie@gmail.com>
2013-06-04 02:43:37 +0200

    Improvments
    Fix syntax
    Better UI (img, code , pre, li)
    Use a workspace instead of lots of zip file

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,5 +2,16 @@ $(function () {
     // Code beautifier
     if (window.prettyPrint && $.isFunction(prettyPrint)) prettyPrint();
 
-    // TODO add dynamic here
+    // add prefix number
+    $("article h2").each(autoPrefix);
+    $(".nav li a").each(autoPrefix);
+
+    // Optional
+    $("a.optional").each(function(idx, elt){
+        $(elt).prepend("<em>(Optional)</em> ");
+    });
 });
+
+var autoPrefix = function(idx, elt){
+    $(elt).prepend("" +(idx+1) + ". ");
+};
